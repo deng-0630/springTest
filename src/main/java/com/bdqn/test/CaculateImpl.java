60059598bfa900bb6db9813a9910aeb78aca6139
deng-0630 <18874600290@163.com>
2020-12-16 08:31:53 +0800

    spring jdbc

--- src/main/java/com/bdqn/test/CaculateImpl.java
+++ src/main/java/com/bdqn/test/CaculateImpl.java
@@ -7,7 +7,7 @@ public class CaculateImpl implements Caculate{
    public int add(int a,int b){
        return a+b;
    }
-    public int sub(int a,int b){
+    public int sub(int     a,int b){
         return a-b;
     }
     public int mul(int a,int b){
